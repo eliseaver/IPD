35338b71d6534cdaf3554b3bc51175a11941d45c
seaver.eli <seaver.eli@asdk12.org>
2020-01-29 08:10:45 -0900

    team 3

--- team3.py
+++ team3.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'Big Money' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'Winning retaliation'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
@@ -18,6 +18,16 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b'. 
     '''
 
+    
+    if their_history == '':
+        return 'b'
+    if their_history[-1] == 'b' and their_history[-2] == 'c':
+        return 'c'
+    else:
+        return 'b'
+
+        
+
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,12 +36,15 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
+   
+        
+    
     return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
+    from s module. Prints error if return value != result.
     Returns True or False, dpending on whether result was as expected.
     '''
     real_result = move(my_history, their_history, my_score, their_score)
